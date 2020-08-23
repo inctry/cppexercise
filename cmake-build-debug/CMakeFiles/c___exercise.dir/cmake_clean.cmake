5d66ac5a1d6e95c717eddb91fa5e9417e40f8449
Inctry <inctry@outlook.com>
2020-08-23 10:32:21 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/c___exercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c___exercise.dir/main.cpp.obj"
+  "c___exercise.exe"
+  "c___exercise.exe.manifest"
+  "c___exercise.pdb"
+  "libc___exercise.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c___exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
